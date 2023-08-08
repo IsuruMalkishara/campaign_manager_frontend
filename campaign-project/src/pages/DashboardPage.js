c560bdd75bebcdfd8521b0ceefa6b45e549d33b1
Malkishara <isurumalkishara97@gmail.com>
2023-08-08 21:01:08 +0530

    navbar added to the dashboard page

--- campaign-project/src/pages/DashboardPage.js
+++ campaign-project/src/pages/DashboardPage.js
@@ -10,15 +10,17 @@ const DashboardPage = () => {
     
 
   return (
-    <div className='dashboard'>
+    <div className='dashboard' style={{ display: 'flex',
+    background: 'rgb(255, 255, 255)',font: 'rgb(0,0,0)',
+    fontFamily:'"Ubuntu", sans-serif;',width: '100%' }}>
     
     <div className='row'>
-          <div className='col-2' style={{ textAlign:'center' }}>
-       <div className='navbar-container'>
+          <div className='col-5' style={{ textAlign:'center' }}>
+       <div className='navbar-container' style={{width: '100%', backgroundColor: 'rgb(0,0,0)',color: 'rgb(255, 255, 255)',padding: '0px'}}>
         <NavbarComponent />
       </div>
       </div>
-      <div  className='col-10'>
+      <div  className='col-7'>
       Dashboard page
       </div>
       </div>
